4b3cb94bb7321426031bb18c1e155640faa622fa
julichka-sun07 <112427883+julichka-sun07@users.noreply.github.com>
2023-10-31 11:35:27 +0300

    ts-hw2-1

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,7 @@
 let some: unknown;
 some = "Text";
 let str: string;
-str = some as string;
-
+if (typeof some === "string") {
+  str = some;
+}
 export {};
--- src/generic/3.ts
+++ src/generic/3.ts
@@ -3,7 +3,7 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
-function merge<T, K>(objA: T, objB: K): T & K {
+function merge<T extends object, K extends object>(objA: T, objB: K): T & K {
   return Object.assign({}, objA, objB);
 }
 
